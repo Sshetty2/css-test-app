ed843efaac97ff7411d952607e0b799325497f70
Sachit <sachit.shetty@gmail.com>
2019-02-25 12:35:08 -0500

    try out different styles based on traversy css intro course on youtube

--- src/App.js
+++ src/App.js
@@ -8,20 +8,19 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <body className='container'>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div className="box-1">
+            <h2>Sample Container</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In delectus mollitia sapiente reiciendis eos laborum! Itaque ipsam cupiditate maiores, possimus minima culpa totam optio et at velit molestias, praesentium architecto.
+            </p>
+          </div>
+        </body>
       </div>
-    );
+    );  
   }
 }
 
